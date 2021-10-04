209b0c1411879efe71c70c423a5303a911e91aad
admin <pun.bxxx@gmail.com>
2021-10-04 14:31:53 +0700

    #pun //console.log(res)

--- frontend/modules/app/views/display/play-sound.php
+++ frontend/modules/app/views/display/play-sound.php
@@ -401,6 +401,7 @@ $("#jplayer_inspector").jPlayerInspector({jPlayer:$(jPlayerid)});
 //Socket Event
 socket
 .on('call', (res) => {
+    console.log(res)
     if(model != null && Object.keys(model).length && myPlaylist.playlist.filter(r => r.title === res.modelQueue.q_num).length === 0){
         var counters = (model.counterserviceid).split(',').map(v => parseInt(v));
         if(jQuery.inArray(parseInt(res.counter.counterserviceid), counters) != -1) {
